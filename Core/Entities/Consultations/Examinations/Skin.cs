a3a9e04534888511e664facf6a6b42f8b1c9ec07
anis hel <anis.helaoui@googlemail.com>
2018-10-21 00:03:16 +0200

    Consultations erweitert

---
+++ Core/Entities/Consultations/Examinations/Skin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Entities.Consultations.Examinations
+{
+    // Peau
+    public class Skin : EntityBase
+    {
+        //Couleur: Bien colorée, paleur cutanée
+        public SkinColor? SkinColor { get; set; }
+        //Cyanose: oui/non
+        public bool? Cyanose { get; set; }
+        //TR : inf/sup à 3 secondes
+        public Tr? Tr { get; set; }
+        //Ictère d´intensité: faible, moyenne, intense
+        public IctereIntensité? IctereIntensité { get; set; }
+        //Eruptions: oui/non(si oui phrase)
+        public bool? Eruption { get; set; }
+        //Eruptions: oui/non(si oui phrase)
+        public string EruptionDescr { get; set; }
+        public Examination Examination { get; set; }
+    }
+}
+
+public enum SkinColor
+{
+    [Display(Name = "bien colorée")]
+    bien_colorée,
+    [Display(Name = "paleur cutanée")]
+    paleur_cutanée
+}
+public enum Tr
+{
+    [Display(Name = "inf à 3 secondes")] 
+    inférieur,
+    [Display(Name = "sup à 3 secondes")]
+    supérieur
+}
+
+public enum IctereIntensité
+{
+    faible,
+    moyenne,
+    intense
+}
---
+++ Core/Entities/Consultations/Mensuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Consultations
+{
+    public class Mensuration : EntityBase
+    {
+        //Poids(en kg.) :
+        public double? Poids { get; set; }
+        //Percentil: entre % 3-97, >, <
+        public string PercentilPoids { get; set; }
+        //Taille(cm) :
+        public double? Taille { get; set; }
+        //Percentil:  entre % 3-97, >, <
+        public string PercentilTaille { get; set; }
+        //PC(cm) :
+        public double? Pc { get; set; }
+        //Percentil en DS: (integer -4, + 3)
+        public double? PercentilPc { get; set; }
+        //Température: (double degrés Célsius)
+        public double? Temperature { get; set; }
+        //IMC: (double 10,5- 50)
+        public double? Imc { get; set; }
+        public Consultation Consultation { get; set; }
+    }
+}
