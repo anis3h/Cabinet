bfd5339846642c51fe52fc14e4c5b2049339757c
anis hel <anis.helaoui@googlemail.com>
2019-10-28 00:12:15 +0100

    Test Azure AD Authentication

--- PatientApi/Startup.cs
+++ PatientApi/Startup.cs
@@ -4,6 +4,8 @@ using CommunCabinet.MapperServices.Interfaces;
 using Core.Interfaces;
 using Core.Services;
 using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -27,10 +29,26 @@ namespace PatientApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             services.AddDbContext<CabinetContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CabinetConnection")).EnableSensitiveDataLogging());
             //services.AddDataAccessServices(Configuration.GetConnectionString("CabinetConnection"));
             services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowOrigin",
+            //        builder => builder.WithOrigins("http://localhost:4200")
+            //            .AllowAnyHeader()
+            //            .AllowAnyMethod());
+            //});
+
+
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -50,6 +68,7 @@ namespace PatientApi
             services.AddScoped<IPatientService, PatientService>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,7 +85,6 @@ namespace PatientApi
                 app.UseExceptionHandler("/Patient/Error");
                 app.UseHsts();
             }
-
             app.UseCors();
             app.UseHttpsRedirection();
             app.UseMvc();
